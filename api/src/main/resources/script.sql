48c22261fce8a7f6f9e5949e96176ec736469ebf
Rob Bertram <robert.bertram@livingasone.com>
2020-08-27 23:45:09 -0500

    fix script

--- api/src/main/resources/script.sql
+++ api/src/main/resources/script.sql
@@ -1,7 +1,14 @@
+
+CREATE TABLE `organization` (
+    `id` VARCHAR(36)  UNIQUE,
+    `name` varchar(155) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `users` (
     `email` varchar(155) UNIQUE,
     `password` varchar(500) DEFAULT NULL,
-    `email` varchar(45) UNIQUE,
     `first_name` varchar(45) DEFAULT NULL,
     `last_name` varchar(45) DEFAULT NULL,
     `organization_fk` varchar(155) DEFAULT NULL,
@@ -12,11 +19,4 @@ CREATE TABLE `users` (
     PRIMARY KEY (`email`),
     FOREIGN KEY(`organization_fk`)
     REFERENCES `organization` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `organization` (
-    `id` VARCHAR(36)  UNIQUE,
-    `name` varchar(155) NOT NULL,
-
-    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
